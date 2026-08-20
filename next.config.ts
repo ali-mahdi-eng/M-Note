c679f5212f656c0a0d8997687fdb3a575d4c47b7
Ali Mahdi <ali.mahdi.engineer@gmail.com>
2026-08-20 11:45:00 +0300

    Update basePath in next.config.ts to use environment variable for flexibility

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/M-Note',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
