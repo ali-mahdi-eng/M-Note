9ebca6e157b6c70de9f339e0e3bea675d5ee756e
Ali Mahdi <ali.mahdi.engineer@gmail.com>
2026-08-31 21:13:03 +0300

    version [0.3.0]
    Refactor application structure: remove unused components, add new features and buttons for note management

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,15 +8,15 @@ export default function manifest(): MetadataRoute.Manifest {
     name: 'M-Note',
     short_name: 'M-Note',
     description: 'A simple note-taking app that allows you to create, edit, and organize your notes with ease. It provides a clean and intuitive interface for managing your notes efficiently.',
-    start_url: '${basePath}/',
-    display: 'fullscreen',
+    start_url: `${basePath}/`,
+    display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#1c1c1c',
     icons: [
-      { src: `${basePath}/icon-192x192.png', sizes: '192x192', type: 'image/png` },
-      { src: `${basePath}/icon-512x512.png', sizes: '512x512', type: 'image/png` },
-      { src: `${basePath}/icon-1024x1024.png', sizes: '1024x1024', type: 'image/png` },
-      { src: `${basePath}/icon-512x512.png', sizes: '512x512', type: 'image/png`, purpose:'maskable' },
+      { src: `${basePath}/icon-192x192.png`, sizes: '192x192', type: 'image/png' },
+      { src: `${basePath}/icon-512x512.png`, sizes: '512x512', type: 'image/png' },
+      { src: `${basePath}/icon-1024x1024.png`, sizes: '1024x1024', type: 'image/png' },
+      { src: `${basePath}/icon-512x512.png`, sizes: '512x512', type: 'image/png', purpose: 'maskable' },
 
      ]
 
